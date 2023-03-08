83117d48c839e48351a1152e77e438c1c4941f7e
GonzaloTuset <soracerito@gmail.com>
2023-03-08 15:31:16 -0300

    utilizacion api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 
 import style from '../src/Appar.module.css'
-
+import { useState } from 'react'
 import Cards from './components/cards/Cards.jsx'
 import SearchBar from './components/searchBar/SearchBar.jsx'
-import characters from './data.js'
+
 
 function App () {
+  const [characters, setCharacters] = useState([])
+  
+  function searchCharacters(character) {
+    fetch(`https://rickandmortyapi.com/api/character/${character}`)
+       .then((response) => response.json())
+       .then((data) => {
+          if (data.name) {
+             setCharacters((oldChars) => [...oldChars, data]);
+          } else {
+             window.alert('No hay personajes con ese ID');
+          }
+       });
+ }
   return (
     <div className={style.div}>
-    <header className={style.header}>npm
+    <header className={style.header}>
       <img className={style.img} src='https://cdn.shopify.com/s/files/1/0346/8063/5529/collections/rick-morty-collection-banner_1400x.jpg?v=1590095280' alt='morit foto'/>
       
     </header>
       <div className={style.search}>
         <SearchBar
-          onSearch={(characterID) => window.alert(characterID)}
+          onSearch={searchCharacters}
         />
       </div>
       
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import style from './search.module.css'
 
-export default function SearchBar(onSearch) {
+export default function SearchBar(pj) {
+   const [id,setID]=useState([]);
+   const handleChange = (event) =>{
+      setID(event.target.value)
+   }
    return (
       <div >
-         <input className={style.input} type='search' />
-      <button className={style.boton} onClick={(id)=>onSearch(id)}>Agregar</button> 
+         <input className={style.input} onChange={handleChange} type='search' />
+      <button className={style.boton} onClick={()=>pj.onSearch(id)}>Agregar</button> 
       </div>
    );
 }
