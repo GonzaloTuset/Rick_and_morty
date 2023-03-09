4c8d9c6be164f0dee03c7c58cec466eb183f37c3
GonzaloTuset <soracerito@gmail.com>
2023-03-09 20:23:35 -0300

    vamo salio re4 demo

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,7 +2,9 @@ import SearchBar from "../searchBar/SearchBar"
 import { Link } from "react-router-dom"
 export default function Nav(props){
   return(
-    
+    <div>
+    <Link to='/home'> <h1>Homi</h1> </Link>
   <SearchBar onSearch = {props.onSearch}/>
+  </div>
   )
 }
